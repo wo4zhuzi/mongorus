bf9acc50f23df6e2dd201dded4689f8d760f789c
david <david@legenddigital.com.au>
2020-12-10 15:20:32 +0800

    logrus mongodb hook init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mongorus
+
+go 1.14
+
+require go.mongodb.org/mongo-driver v1.4.4 // indirect
---
+++ mongorus.go
@@ -0,0 +1,64 @@
+package mongorus
+
+import (
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client
+
+type M bson.M
+
+type MongoHook struct {
+	MongoCollection *mongo.Collection
+}
+
+func NewMongoHook(mongoUrl, db, collection string) (*MongoHook, error) {
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s",mongoUrl))
+
+	var err error
+	MongoClient, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MongoHook{MongoCollection: MongoClient.Database(db).Collection(collection)}, nil
+}
+
+func NewAuthMongoHook(mongoUrl, db, collection string, auth options.Credential) (*MongoHook, error) {
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s",mongoUrl)).SetAuth(auth)
+
+	var err error
+	MongoClient, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MongoHook{MongoCollection: MongoClient.Database(db).Collection(collection)}, nil
+}
+
+func (h *MongoHook)Fire(entry *logrus.Entry) error{
+	data := make(logrus.Fields)
+	data["Level"] = entry.Level.String()
+	data["Time"] = entry.Time
+	data["Message"] = entry.Message
+
+	for k, filed := range entry.Data {
+		data[k] = filed
+	}
+
+	_, err := h.MongoCollection.InsertOne(context.Background(), M(data))
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *MongoHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
